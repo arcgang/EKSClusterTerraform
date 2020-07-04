2ebca8f557643995475d51c77ba34e256f19678e
Archan Ganguly <archan@Archans-MacBook-Pro.local>
2020-07-04 12:37:52 +0200

    output variables added

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "region" {
 
 output "vpc_id" {
     description = "VPC"
-    value = module.vpc.id
+    value = module.vpc.vpc_id
 }
 
 output "security_group_ids" {
     description = "Security Groups"
-    value = [aws_security_group.all_worker_mgmt.id, aws_security_group.worker_group_mgmt_two]
+    value = [aws_security_group.all_worker_mgmt.id, aws_security_group.worker_group_mgmt_two.id]
 }
