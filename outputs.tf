342ba94dad02e8aa6d2142ae5d2c112ec71468b4
Archan Ganguly <archan@Archans-MacBook-Pro.local>
2020-07-04 10:13:40 +0200

    test

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
-
 output "region" {
   description = "AWS region"
   value       = var.region
