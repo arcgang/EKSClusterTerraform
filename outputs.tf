c1433181f11d3554bf161b0dfb0ad94a026c861a
Archan Ganguly <archan@Archans-MacBook-Pro.local>
2020-07-04 13:22:01 +0200

    test

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,13 @@ output "security_group_ids" {
     description = "Security Groups"
     value = [aws_security_group.all_worker_mgmt.id, aws_security_group.worker_group_mgmt_two.id]
 }
+
+output "private_subnets" {
+    description = "Private Subnets"
+    value = module.vpc.private_subnets
+}
+
+output "vpc_cidr_block" {
+    description = "VPC CIDR block"
+    value = module.vpc.cidr
+}
